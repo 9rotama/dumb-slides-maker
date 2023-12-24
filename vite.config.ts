ac2922043c0b895b5fcabb80a55e99e38a3c5f6c
9rotama <65887771+9rotama@users.noreply.github.com>
2023-12-24 22:05:03 +0900

    unocssのsvelte scopedプラグインインストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import UnoCSS from '@unocss/svelte-scoped/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), UnoCSS()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
