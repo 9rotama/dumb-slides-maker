36f433a4da3bb693756c32b0befdd1fdf62e0eda
9rotama <65887771+9rotama@users.noreply.github.com>
2024-01-01 16:18:24 +0900

    テキストエディタの初期データ変更

--- src/stores/markdownTextStore.ts
+++ src/stores/markdownTextStore.ts
@@ -1,3 +1,27 @@
 import { writable } from 'svelte/store';
 
-export const markdownTextStore = writable<string>('# タイトル');
+export const markdownTextStore = writable<string>(`
+---
+paginate: true
+header: ""
+footer: ""
+---
+
+<!--
+_class: lead
+_paginate: false
+_header: ""
+_footer: ""
+-->
+
+# タイトル
+
+名前
+
+---
+
+# タイトル
+
+![width:200px image](https://i.imgur.com/gYoEV48.jpeg)
+
+`);
